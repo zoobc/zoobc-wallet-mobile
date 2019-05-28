9261e023eaa3a1ad28cc5094be6def137866a30f
jhonkus <putukn@gmail.com>
2019-05-28 17:28:16 +0800

    Modif wallet view

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,54 @@
 import { Component } from '@angular/core';
+import {
+  BarcodeScannerOptions,
+  BarcodeScanner
+} from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+
+export class Tab3Page {
+  encodeData: any;
+  scannedData:{};
+  barcodeScannerOptions: BarcodeScannerOptions;
+
+  constructor(private barcodeScanner: BarcodeScanner) {
+    this.encodeData = '1Lhqun1E9zZZhodiTqxfPQBcwr1CVDV2sy';  
+    // Options
+    this.barcodeScannerOptions = {
+      showTorchButton: true,
+      showFlipCameraButton: true
+    };
+  }
+
+  scanCode() {
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        //alert('Barcode data ' + JSON.stringify(barcodeData));
+        this.scannedData = barcodeData;
+      })
+      .catch(err => {
+        console.log('Error', err);
+      });
+  }
+
+  
+  encodedText() {
+    this.barcodeScanner
+      .encode(this.barcodeScanner.Encode.TEXT_TYPE, this.encodeData)
+      .then(
+        encodedData => {
+          console.log(encodedData);
+          this.encodeData = encodedData;
+        },
+        err => {
+          console.log('Error occured : ' + err);
+        }
+      );
+  }
+
+}
