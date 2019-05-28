8cb6c5d1b107a4817a32185fc374a72a70d11bc3
jhonkus <putukn@gmail.com>
2019-05-28 17:30:15 +0800

    Modif send view

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,57 @@
 import { Component } from '@angular/core';
+import {
+  BarcodeScannerOptions,
+  BarcodeScanner
+} from '@ionic-native/barcode-scanner/ngx';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+
+export class Tab2Page {
+  encodeData: any;
+  scannedData:{};
+  barcodeScannerOptions: BarcodeScannerOptions;
+
+
+  constructor(private barcodeScanner: BarcodeScanner) {
+    this.encodeData = 'yuhjKhgjhgdOp786579954jhfjkhkk';
+  
+    // Options
+    this.barcodeScannerOptions = {
+      showTorchButton: true,
+      showFlipCameraButton: true
+    };
+  }
+
+  scanCode() {
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        alert('Barcode data ' + JSON.stringify(barcodeData));
+        this.scannedData = barcodeData;
+      })
+      .catch(err => {
+        console.log('Error', err);
+      });
+  }
+
+  encodedText() {
+    this.barcodeScanner
+      .encode(this.barcodeScanner.Encode.TEXT_TYPE, this.encodeData)
+      .then(
+        encodedData => {
+          console.log(encodedData);
+          this.encodeData = encodedData;
+        },
+        err => {
+          console.log('Error occured : ' + err);
+        }
+      );
+  }
+
+
+}
