8fd4975a221a9db1172de83d59dfc7a1f3d0a28f
Agus Suryantara <helloaguss@gmail.com>
2019-05-31 13:15:15 +0800

    bug fix

--- src/app/setup-pin/setup-pin.page.ts
+++ src/app/setup-pin/setup-pin.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import sha256 from 'crypto-js/sha256';
 
 @Component({
   selector: 'app-setup-pin',
@@ -7,6 +8,7 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['./setup-pin.page.scss'],
 })
 export class SetupPinPage implements OnInit {
+  pin: number
 
   constructor(
     private storage: Storage
@@ -15,8 +17,10 @@ export class SetupPinPage implements OnInit {
   ngOnInit() {
   }
 
-  savePin() {
-    this.storage.set("test", "value")
+  async savePin() {
+    const encryptedPin = sha256(this.pin).toString();
+    this.storage.set("pin", encryptedPin)
+    const a = await this.storage.get("pin")
   }
 
 }
