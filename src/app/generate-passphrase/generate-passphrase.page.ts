79eaca1421534897befe6ef0d7eb92ae9a8c8563
Agus Suryantara <helloaguss@gmail.com>
2019-06-11 16:34:54 +0800

    update login flow & encrypt pin and private key

--- src/app/generate-passphrase/generate-passphrase.page.ts
+++ src/app/generate-passphrase/generate-passphrase.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { KeyringService } from '../core/keyring.service';
+import { ToastController } from '@ionic/angular';
+import { MnemonicsService } from '../core/mnemonics.service';
 
 @Component({
   selector: 'app-generate-passphrase',
@@ -10,18 +13,61 @@ export class GeneratePassphrasePage implements OnInit {
   private writtenDown = false
   private terms = false
   private setupPin = false
+  private passphrase
 
 
-  constructor() { }
+  constructor(
+    private keyringService: KeyringService,
+    private toastController: ToastController,
+    private mnemonicsService: MnemonicsService
+  ) { }
 
   ngOnInit() {
-    this.checkSetupPin();
+    this.checkSetupPin()
+    this.generatePassphrase()
   }
 
   checkSetupPin() {
     if(this.writtenDown && this.terms) {
       this.setupPin = true
     }
+  }
+
+  generatePassphrase() {
+    this.passphrase = this.keyringService.generateRandomPhrase().phrase
+    const privateKey = this.mnemonicsService.mnemonic.toSeed(this.passphrase, null)
+
+    console.log("privateKey", privateKey)
+  }
+
+  copyToClipboard() {
+    const val = this.passphrase
+
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+
+    this.copyToast()
+  }
+
+  async copyToast() {
+    const toast = await this.toastController.create({
+      message: 'Passphrase copied to clipboard',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  test() {
+    this.mnemonicsService.mnemonic.toSeed(this.passphrase, null)
   }
 
 }
--- src/app/setup-pin/setup-pin.page.ts
+++ src/app/setup-pin/setup-pin.page.ts
@@ -3,6 +3,8 @@ import { Storage } from '@ionic/storage';
 import sha512 from 'crypto-js/sha512';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/services/auth-service';
+import { CryptoService } from 'src/services/crypto.service';
+import { ConverterService } from 'src/services/converter.service';
 
 @Component({
   selector: 'app-setup-pin',
@@ -15,10 +17,47 @@ export class SetupPinPage implements OnInit {
   constructor(
     private storage: Storage,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private cryptoService: CryptoService,
+    private converterService: ConverterService
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.testFunction();
+  }
+
+  async testFunction() {
+    const pin = "1234"
+    const seedHex = "86d7324f71d22f56f244a4b2ca37000cbcb429fdcd173139534bc8526dfbebb29fb15e09bba7235100e1e89f33dcc9629e6286103aec559c10efaf22558c3474"
+
+    const privKeyUint8 = await this.cryptoService.rootKeyFromSeed(this.converterService.hexToArrayByte(seedHex))
+    console.log("passphraseUint8", privKeyUint8)
+    const pinUint8 = this.converterService.stringToArrayByte(pin)
+    console.log("pinUI8", pinUint8)
+
+    const keyAlgo = {
+      name: 'AES-GCM',
+      length: 256,
+    }
+
+    const opts = {
+      format: 'jwk',
+      keyAlgo,
+      wrapAlgo: {
+        name: 'AES-GCM',
+        iv: this.cryptoService.genInitVector(),
+      },
+      deriveAlgo: this.cryptoService.genDeriveAlgo(),
+    }
+
+    console.log("opts", opts)
+
+    const test = await this.cryptoService.wrapKeyWithPin(privKeyUint8, pinUint8, opts)
+    console.log("huhuhu", test)
+    // this.cryptoService.wrapKeyWithPin()
+
+    this.storage.set('encrypted_key', test)
+  }
 
   async savePin() {
     console.log("test")
