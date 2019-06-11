f0fa6cc8012ce5ba4363a154a0cd7ca534dd861a
Agus Suryantara <helloaguss@gmail.com>
2019-06-11 16:36:25 +0800

    remove console.log

--- src/app/setup-pin/setup-pin.page.ts
+++ src/app/setup-pin/setup-pin.page.ts
@@ -31,9 +31,7 @@ export class SetupPinPage implements OnInit {
     const seedHex = "86d7324f71d22f56f244a4b2ca37000cbcb429fdcd173139534bc8526dfbebb29fb15e09bba7235100e1e89f33dcc9629e6286103aec559c10efaf22558c3474"
 
     const privKeyUint8 = await this.cryptoService.rootKeyFromSeed(this.converterService.hexToArrayByte(seedHex))
-    console.log("passphraseUint8", privKeyUint8)
     const pinUint8 = this.converterService.stringToArrayByte(pin)
-    console.log("pinUI8", pinUint8)
 
     const keyAlgo = {
       name: 'AES-GCM',
@@ -50,17 +48,13 @@ export class SetupPinPage implements OnInit {
       deriveAlgo: this.cryptoService.genDeriveAlgo(),
     }
 
-    console.log("opts", opts)
-
     const test = await this.cryptoService.wrapKeyWithPin(privKeyUint8, pinUint8, opts)
-    console.log("huhuhu", test)
     // this.cryptoService.wrapKeyWithPin()
 
     this.storage.set('encrypted_key', test)
   }
 
   async savePin() {
-    console.log("test")
     const encryptedPin = sha512(this.pin.toString()).toString()
     this.storage.set("pin", encryptedPin)
     const isUserLoggedIn = await this.authService.login(this.pin)
