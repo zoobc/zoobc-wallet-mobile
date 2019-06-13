4dcb3d78f1c654d08afa2e59d60a68d24a469075
Heri Sim <527101+heri16@users.noreply.github.com>
2019-06-13 19:34:25 +0800

    fix converters bufferOffset

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -1,24 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { KeyringService } from '../core/keyring.service';
 
-import { hexToArrayByte, arrayByteToHex, publicKeyToAddress } from '../../helpers/converters'
-
+import { hexToByteArray, byteArrayToHex, publicKeyToAddress } from '../../helpers/converters'
 // import { calcBip32ExtendedKey } from '../core/keyring.service';
-// import { getMasterKeyFromSeed, derivePath } from '../../../externals/hd-key';
 
-// export function arrayByteToHex(bytes) {
-//   return bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
-// }
-
-// export function stringToArrayByte(str) {
-//   str = unescape(encodeURIComponent(str)); //temporary
-
-//   var bytes = new Array(str.length);
-//   for (var i = 0; i < str.length; ++i)
-//       bytes[i] = str.charCodeAt(i);
-
-//   return Uint8Array.from(bytes);
-// }
+const coinName = "SPN"
 
 @Component({
   selector: 'app-test',
@@ -33,7 +19,7 @@ export class TestPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    console.log("bip32")
+    console.log("bip44 coinName:", coinName)
   }
 
   testService() {
@@ -41,33 +27,35 @@ export class TestPage implements OnInit {
     console.log("new phrase:", phrase)
 
     phrase = "cable spray genius state float twenty onion head street palace net private method loan turn phrase state blanket interest dry amazing dress blast tube"
-    console.log("phrase:", phrase)
+    const passphrase = "p4ssphr4se"
 
-    const { seed, bip32RootKey } = this.keyringService.calcBip32RootKeyFromSeed("SPN", phrase, "p4ssphr4se")
-    console.log("seed:", seed)
-    console.log("bip32RootKey:", bip32RootKey)
-    // console.log("bip32RootKey:", getMasterKeyFromSeed(seed))
+    console.log("masterPhrase:", phrase)
+    console.log("masterPassphrase:", passphrase)
+
+    const { seed, bip32RootKey } = this.keyringService.calcBip32RootKeyFromSeed(coinName, phrase, passphrase)
+    console.log("masterSeed:", seed)
+    console.log("rootKey:", bip32RootKey)
 
     // console.log("sep5TestVector:", calcBip32ExtendedKey("m/44'/148'", bip32RootKey, "ed25519"))
-    // console.log("sep5TestVector:", derivePath("m/44'/148'", seed))
 
-    const account0 = this.keyringService.calcForDerivationPathForCoin("SPN", 0, 0, bip32RootKey)
+    const account0 = this.keyringService.calcForDerivationPathForCoin(coinName, 0, 0, bip32RootKey)
     console.log("account0:", account0)
-    // console.log("account0:", derivePath("m/44'/148'/0'", seed))
 
     // const account1 = this.keyringService.calcForDerivationPathForCoin("BTC", 1, 0, bip32RootKey)
     // console.log("account1:", account1)
 
-    console.debug(arrayByteToHex(this.keyringService.extendedSeed))
-    const { publicKey, secretKey } = this.sign.keyPair.fromSeed(this.keyringService.extendedSeed)
-    console.log("account m/44'/148'/0' secretKey:", arrayByteToHex(secretKey))
-    console.log("account m/44'/148'/0' publicKey:", arrayByteToHex(publicKey))
-    console.log("account m/44'/148'/0' address:", publicKeyToAddress(secretKey))
+    const { derivationPrivKey: accountSeed } = account0
+    const { publicKey, secretKey } = this.sign.keyPair.fromSeed(accountSeed)
+    console.log("account m/44'/148'/0' privateKey:", byteArrayToHex(accountSeed))
+    console.log("account m/44'/148'/0' publicKey:", byteArrayToHex(publicKey))
+    console.log("account m/44'/148'/0' address:", publicKeyToAddress(publicKey))
 
-    const txBytes = hexToArrayByte("0100018407025d3c00000004264abef89b96225a837f9d6a2ccc09e8b1422e090c0fa3852bb139d99caec404264a2ef814619d4a2b1fa3b45f4aa09b248d53ef07d8e92237f3cc8eb30d6d809698000000000000e1f5050000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004264a2ef814619d4a2b1fa3b45f4aa09b248d53ef07d8e92237f3cc8eb30d6d809698000000000000e1f50500000000")
-    console.log("sign data:", arrayByteToHex(txBytes))
+    const txBytes = hexToByteArray("0100018407025d3c00000004264abef89b96225a837f9d6a2ccc09e8b1422e090c0fa3852bb139d99caec404264a2ef814619d4a2b1fa3b45f4aa09b248d53ef07d8e92237f3cc8eb30d6d809698000000000000e1f5050000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004264a2ef814619d4a2b1fa3b45f4aa09b248d53ef07d8e92237f3cc8eb30d6d809698000000000000e1f50500000000")
+    console.log("unsignedTxBytes:", byteArrayToHex(txBytes))
     const signature = this.sign.detached(txBytes, secretKey)
-    console.log("account m/44'/148'/0' signature:", arrayByteToHex(signature))
+    console.log("txSignature:", byteArrayToHex(signature))
+    txBytes.set(signature, 123)
+    console.log("signedTxBytes:", byteArrayToHex(txBytes))
   }
 
 }
--- src/helpers/converters.ts
+++ src/helpers/converters.ts
@@ -1,40 +1,43 @@
-export function hexToArrayByte(hexStr : string) : Uint8Array {
+export function hexToByteArray(hexStr : string) : Uint8Array {
   return new Uint8Array(hexStr.match(/[\da-f]{2}/gi).map(byte => parseInt(byte, 16)));
 }
 
-export function arrayByteToHex(bytes: ArrayBuffer | ArrayBufferView | Array<number>) : string {
-  const byteArray = bytes instanceof ArrayBuffer ? bytes : ArrayBuffer.isView(bytes) ? new Uint8Array(bytes.buffer) : Uint8Array.from(bytes);
+export function byteArrayToHex(bytes: ArrayBuffer | ArrayBufferView | Array<number>) : string {
+  const byteArray = bytes instanceof ArrayBuffer ? new Uint8Array(bytes) : ArrayBuffer.isView(bytes) ? new Uint8Array(bytes.buffer, bytes.byteOffset, bytes.byteLength) : Uint8Array.from(bytes);
   return Array.prototype.map.call(byteArray, byte => byte.toString(16).padStart(2, "0")).join('');
 }
 
 /*
  * See: https://developers.google.com/web/updates/2014/08/Easier-ArrayBuffer-String-conversion-with-the-Encoding-API
  */
-export function stringToArrayByte(str: string) : Uint8Array {
+export function stringToByteArray(str: string) : Uint8Array {
   const encoder = new TextEncoder();
   return encoder.encode(str);
 }
 
-export function arrayByteToString(bytes: ArrayBuffer | ArrayBufferView | Array<number>) : string {
+export function byteArrayToString(bytes: ArrayBuffer | ArrayBufferView | Array<number>) : string {
   const byteArray = bytes instanceof ArrayBuffer ? bytes : ArrayBuffer.isView(bytes) ? bytes : Uint8Array.from(bytes);
   const decoder = new TextDecoder();
   return decoder.decode(byteArray);
 }
 
-export function base64ToArrayByte(base64Str: string) : Uint8Array {
+export function base64ToByteArray(base64Str: string) : Uint8Array {
   const buf = new Buffer(base64Str, 'base64');
-  return new Uint8Array(buf.buffer);
+  return new Uint8Array(buf.buffer, buf.byteOffset, buf.byteLength);
 }
 
-export function arrayByteToBase64(bytes: ArrayBuffer | ArrayBufferView | Array<number>) : string {
-  // const byteArray = bytes instanceof ArrayBuffer ? new Uint8Array(bytes) : ArrayBuffer.isView(bytes) ? new Uint8Array(bytes.buffer) : Uint8Array.from(bytes);
+export function byteArrayToBase64(bytes: ArrayBuffer | ArrayBufferView | Array<number>) : string {
+  // const byteArray = bytes instanceof ArrayBuffer ? new Uint8Array(bytes) : ArrayBuffer.isView(bytes) ? new Uint8Array(bytes.buffer, bytes.byteOffset, bytes.byteLength) : Uint8Array.from(bytes);
   // return btoa([].reduce.call(byteArray, (acc, byte) => acc + String.fromCharCode(byte), ''));
-  const arrayBuf : ArrayBufferLike = bytes instanceof ArrayBuffer ? bytes : ArrayBuffer.isView(bytes) ? bytes.buffer : Uint8Array.from(bytes).buffer;
-  const buf = Buffer.from(arrayBuf);
+  const buf = bytes instanceof ArrayBuffer ? Buffer.from(bytes) : ArrayBuffer.isView(bytes) ? Buffer.from(bytes.buffer, bytes.byteOffset, bytes.byteLength) : Buffer.from(bytes);
   return buf.toString('base64');
 }
 
-export function mergeTypedArrays(resultConstructor, arrays: Array<ArrayBufferView | number[]>) {
+export function toBase64Url(base64Str: string) : string {
+  return base64Str.replace(/\+/g, '-').replace(/\//g, '_');
+}
+
+export function mergeByteArrays(resultConstructor, arrays: Array<ArrayBufferView | number[]>) {
   let totalLength = arrays.reduce((acc, arr) => ((arr as ArrayBufferView).byteLength || (arr as number[]).length) + acc, 0);
   let result = new resultConstructor(totalLength);
   let offset = 0;
@@ -47,14 +50,14 @@ export function mergeTypedArrays(resultConstructor, arrays: Array<ArrayBufferVie
 }
 
 export function publicKeyToAddress(hexOrBytes: string | ArrayBuffer | ArrayBufferView | Array<number>) : string {
-  const bytes = (typeof hexOrBytes === 'string') ? hexToArrayByte(hexOrBytes) : hexOrBytes instanceof ArrayBuffer ? new Uint8Array(hexOrBytes) : ArrayBuffer.isView(hexOrBytes) ? hexOrBytes : Uint8Array.from(hexOrBytes);
+  const bytes = (typeof hexOrBytes === 'string') ? hexToByteArray(hexOrBytes) : hexOrBytes instanceof ArrayBuffer ? new Uint8Array(hexOrBytes) : ArrayBuffer.isView(hexOrBytes) ? hexOrBytes : Uint8Array.from(hexOrBytes);
 
-  const addressBytes = mergeTypedArrays(Uint8Array, [bytes, getAddressChecksum(bytes)]);
-  return arrayByteToBase64(addressBytes);
+  const addressBytes = mergeByteArrays(Uint8Array, [bytes, getAddressChecksum(bytes)]);
+  return toBase64Url(byteArrayToBase64(addressBytes));
 }
 
-export function getAddressChecksum(bytes: ArrayBuffer | SharedArrayBuffer | ArrayBufferView) : Uint8Array {
-  const view = new DataView(ArrayBuffer.isView(bytes) ? bytes.buffer : bytes);
+export function getAddressChecksum(bytes: ArrayBuffer | ArrayBufferView | Array<number>) : Uint8Array {
+  const view : DataView = bytes instanceof ArrayBuffer ? new DataView(bytes) : ArrayBuffer.isView(bytes) ? new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength) : new DataView(Uint8Array.from(bytes).buffer);
   const n = view.byteLength;
   let a = 0;
   for (let i = 0; i < n; i++) {
