20a4bbc4ca9354e64c52ff79de8291c4b7f726ea
Heri Sim <527101+heri16@users.noreply.github.com>
2019-06-17 17:49:38 +0800

    add SendMoneyTx Byte-serializer

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { KeyringService } from '../core/keyring.service';
 
-import { hexToByteArray, byteArrayToHex, publicKeyToAddress } from '../../helpers/converters'
+import { byteArrayToHex, publicKeyToAddress, addressToPublicKey } from '../../helpers/converters'
+import { SendMoneyTx } from '../../helpers/serializers';
+
 // import { calcBip32ExtendedKey } from '../core/keyring.service';
 
 const coinName = "SPN"
@@ -32,26 +34,37 @@ export class TestPage implements OnInit {
     console.log("masterPhrase:", phrase)
     console.log("masterPassphrase:", passphrase)
 
+
+    // Root-Key Operation
     const { seed, bip32RootKey } = this.keyringService.calcBip32RootKeyFromSeed(coinName, phrase, passphrase)
     console.log("masterSeed:", seed)
     console.log("rootKey:", bip32RootKey)
 
     // console.log("sep5TestVector:", calcBip32ExtendedKey("m/44'/148'", bip32RootKey, "ed25519"))
 
+    // Child-Key/Seed operation
     const account0 = this.keyringService.calcForDerivationPathForCoin(coinName, 0, 0, bip32RootKey)
     console.log("account0:", account0)
 
-    // const account1 = this.keyringService.calcForDerivationPathForCoin("BTC", 1, 0, bip32RootKey)
-    // console.log("account1:", account1)
-
+    // Ed25519-KeyPair operation
     const { derivationPrivKey: accountSeed } = account0
     const { publicKey, secretKey } = this.sign.keyPair.fromSeed(accountSeed)
     console.log("account m/44'/148'/0' privateKey:", byteArrayToHex(accountSeed))
     console.log("account m/44'/148'/0' publicKey:", byteArrayToHex(publicKey))
     console.log("account m/44'/148'/0' address:", publicKeyToAddress(publicKey))
 
-    const txBytes = hexToByteArray("0100018407025d3c00000004264abef89b96225a837f9d6a2ccc09e8b1422e090c0fa3852bb139d99caec404264a2ef814619d4a2b1fa3b45f4aa09b248d53ef07d8e92237f3cc8eb30d6d809698000000000000e1f5050000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004264a2ef814619d4a2b1fa3b45f4aa09b248d53ef07d8e92237f3cc8eb30d6d809698000000000000e1f50500000000")
+    // Generate-TX operation
+    // const tx = SendMoneyTx.from({ amount: 1, fee: 1 });
+    const tx = new SendMoneyTx();
+    tx.senderPublicKey = publicKey;
+    tx.recipientPublicKey = addressToPublicKey("RmKSf9_h-v1jW59kOsyfhoIVBKLXGOv91E9lgMIx25vh");
+    tx.amount = 1;
+    tx.fee = 1;
+    tx.timestamp = Date.now() / 1000;
+    const txBytes = tx.toBytes();
     console.log("unsignedTxBytes:", byteArrayToHex(txBytes))
+
+    // Sign-TX operation
     const signature = this.sign.detached(txBytes, secretKey)
     console.log("txSignature:", byteArrayToHex(signature))
     txBytes.set(signature, 123)
