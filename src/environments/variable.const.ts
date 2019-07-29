3e0661180f731a1bf23a50aefaa836aaa491b774
jhonkus <putukn@gmail.com>
2019-07-29 15:00:53 +0800

    Add currency services

--- src/environments/variable.const.ts
+++ src/environments/variable.const.ts
@@ -1,15 +1,28 @@
-export const ACTIVE_ACCOUNT = 'ACTIVE_ACCOUNT'
-
+export const ACTIVE_ACCOUNT = 'ACTIVE_ACCOUNT';
 
 // Language
-export const SELECTED_LANGUAGE = 'SELECTED_LANGUAGE'
+export const SELECTED_LANGUAGE = 'SELECTED_LANGUAGE';
 export const LANGUAGES = [
-    {
-        country: 'English',
-        code: 'en'
-    },
-    {
-        country: 'Indonesia',
-        code: 'id'
-    },
-]
+  {
+    country: 'English',
+    code: 'en'
+  },
+  {
+    country: 'Indonesia',
+    code: 'id'
+  }
+];
+
+// Currency
+export const SELECTED_CURRENCY = 'SELECTED_CURRENCY';
+export const CURRENCIES = [
+  {
+    country: 'United State',
+    code: 'USD'
+  },
+  {
+    country: 'Indonesia',
+    code: 'IDR'
+  }
+];
+export const RATES = 'RATES';
