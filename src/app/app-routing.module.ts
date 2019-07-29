eda8c38051741f9ea1b095d76cfa06f3494ba2c1
Ary Atmajaya <aryatmajaya@gmail.com>
2019-07-29 16:21:08 +0800

    create node admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,8 @@ const routes: Routes = [
   { path: 'test-w', loadChildren: './test-w/test-w.module#TestWPageModule' },
   { path: 'existing-wallet', loadChildren: './existing-wallet/existing-wallet.module#ExistingWalletPageModule' },
   { path: 'test', loadChildren: './test/test.module#TestPageModule' },
-  { path: 'create-account', loadChildren: './create-account/create-account.module#CreateAccountPageModule' }
+  { path: 'create-account', loadChildren: './create-account/create-account.module#CreateAccountPageModule' },
+  { path: 'node-admin', loadChildren: './node-admin/node-admin.module#NodeAdminModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/node-admin/node-admin.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-node-admin',
+  templateUrl: './node-admin.component.html',
+  styleUrls: ['./node-admin.component.scss'],
+})
+export class NodeAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
