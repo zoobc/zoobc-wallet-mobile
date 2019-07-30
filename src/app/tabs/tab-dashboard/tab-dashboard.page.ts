cca399d19e05c9b0d2fef6b269d2f17b4aa3e264
Ary Atmajaya <aryatmajaya@gmail.com>
2019-07-30 13:26:12 +0800

    use dummy data for dashboard

--- src/app/tabs/tab-dashboard/tab-dashboard.page.ts
+++ src/app/tabs/tab-dashboard/tab-dashboard.page.ts
@@ -1,25 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   LoadingController,
   MenuController,
   NavController
-} from '@ionic/angular';
-import { RestapiService } from '../../../services/restapi.service';
-import { AuthService } from 'src/services/auth-service';
-import { Router } from '@angular/router';
-import { AccountService } from 'src/services/account.service';
-import { GRPCService } from 'src/services/grpc.service';
+} from "@ionic/angular";
+import { RestapiService } from "../../../services/restapi.service";
+import { AuthService } from "src/services/auth-service";
+import { Router } from "@angular/router";
+import { AccountService } from "src/services/account.service";
+import { GRPCService } from "src/services/grpc.service";
 @Component({
-  selector: 'app-tab-dashboard',
-  templateUrl: 'tab-dashboard.page.html',
-  styleUrls: ['tab-dashboard.page.scss']
+  selector: "app-tab-dashboard",
+  templateUrl: "tab-dashboard.page.html",
+  styleUrls: ["tab-dashboard.page.scss"]
 })
 export class TabDashboardPage implements OnInit {
   data1: any;
   data2: any;
-  balance: any;
-  transactions: any;
-  publicKey = 'JkhkUiury9899';
+
+  publicKey = "JkhkUiury9899";
+
+  balance = 18.0;
+  unconfirmedBalance = 10.0;
+  transactions = [
+    {
+      title: "otAT-ML-eTz6nrHppp8kWmPCVBv9f5cGDavTwyZanYeG",
+      date: "18 Jul 2019",
+      type: "plus",
+      amount: 200
+    },
+    {
+      title: "otAT-ML-eTz6nrHppp8kWmPCVBv9f5cGDavTwyZanYeG",
+      date: "10 Jul 2019",
+      type: "minus",
+      amount: 10
+    },
+    {
+      title: "otAT-ML-eTz6nrHppp8kWmPCVBv9f5cGDavTwyZanYeG",
+      date: "8 Jul 2019",
+      type: "minus",
+      amount: 90
+    },
+    {
+      title: "otAT-ML-eTz6nrHppp8kWmPCVBv9f5cGDavTwyZanYeG",
+      date: "20 Jun 2019",
+      type: "plus",
+      amount: 100
+    }
+  ];
 
   constructor(
     private apiservice: RestapiService,
@@ -32,23 +60,23 @@ export class TabDashboardPage implements OnInit {
   ) {}
 
   goToSend() {
-    this.router.navigateByUrl('tabs/send');
+    this.router.navigateByUrl("tabs/send");
   }
 
   goToRequest() {
-    this.router.navigateByUrl('tabs/receive');
+    this.router.navigateByUrl("tabs/receive");
   }
 
   async getBalance(pKey: string) {
     const loading = await this.loadingController.create({
-      message: 'Loading'
+      message: "Loading"
     });
     await loading.present();
     this.apiservice.getBalance(pKey).subscribe(
       res => {
         console.log(res);
         this.data1 = res[0];
-        this.balance = this.data1['data'];
+        this.balance = this.data1["data"];
         loading.dismiss();
       },
       err => {
@@ -60,14 +88,14 @@ export class TabDashboardPage implements OnInit {
 
   async getTransaction(pKey: string) {
     const loading = await this.loadingController.create({
-      message: 'Loading'
+      message: "Loading"
     });
     await loading.present();
     this.apiservice.getAccountTransactions(pKey).subscribe(
       res => {
         console.log(res);
         this.data2 = res[0];
-        this.transactions = this.data2['transactions'];
+        this.transactions = this.data2["transactions"];
         loading.dismiss();
       },
       err => {
@@ -78,27 +106,27 @@ export class TabDashboardPage implements OnInit {
   }
 
   openMenu() {
-    this.menuController.open('mainMenu');
+    this.menuController.open("mainMenu");
   }
 
   logout() {
     this.authService.logout();
-    this.router.navigate(['login']);
+    this.router.navigate(["login"]);
   }
 
   ngOnInit() {
     // this.getBalance(this.publicKey);
     // this.getTransaction(this.publicKey);
-    this.getAccountBalance();
-    this.getAccountTransaction();
+    //this.getAccountBalance();
+    //this.getAccountTransaction();
   }
 
   async getAccountBalance() {
-    this.balance = await this.grpcService.getAccountBalance();
-    console.log('balance', this.balance);
+    //this.balance = await this.grpcService.getAccountBalance();
+    //console.log("balance", this.balance);
   }
 
   async getAccountTransaction() {
-    this.transactions = await this.grpcService.getAccountTransaction();
+    //this.transactions = await this.grpcService.getAccountTransaction();
   }
 }
