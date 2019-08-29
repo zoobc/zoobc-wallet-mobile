796dfccfaaa137359c6d41eb9a3b3dbab04a6335
Ary Atmajaya <aryatmajaya@gmail.com>
2019-08-29 18:17:16 +0800

    update setup pin on generate passphrase

--- src/app/Pages/generate-passphrase/generate-passphrase.page.ts
+++ src/app/Pages/generate-passphrase/generate-passphrase.page.ts
@@ -1,18 +1,9 @@
-import { Component, OnInit, Inject } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { KeyringService } from "../../Services/keyring.service";
 import { ToastController, NavController } from "@ionic/angular";
-import { MnemonicsService } from "../../Services/mnemonics.service";
-import { Storage } from "@ionic/storage";
-import { CryptoService } from "src/app/Services/crypto.service";
-import { ConverterService } from "src/app/Services/converter.service";
-import { Router } from "@angular/router";
-import { ObservableService } from "src/app/Services/observable.service";
-import { ACTIVE_ACCOUNT } from "src/environments/variable.const";
 import { CreateAccountService } from "../../Services/create-account.service";
 import { AuthService } from "src/app/Services/auth-service";
-// import * as bip32 from 'bip32'
-// import { calcBip32ExtendedKey } from '../Services/keyring.service';
-// import { arrayByteToHex, stringToArrayByte } from '../../helpers/converters'
+import { SetupPinService } from "src/app/Services/setup-pin.service";
 
 @Component({
   selector: "app-generate-passphrase",
@@ -23,100 +14,40 @@ export class GeneratePassphrasePage implements OnInit {
   writtenDown = false;
   terms = false;
   passphrase: string;
-  isPinSetup = false;
-
-  private account;
-
-  pagePosition: number = 1;
-  pageStep: number = 1;
-  tempPin: string = "";
 
   constructor(
     private keyringService: KeyringService,
     private toastController: ToastController,
-    private mnemonicsService: MnemonicsService,
-    private storage: Storage,
-    private cryptoService: CryptoService,
-    private converterService: ConverterService,
-    private Obs: ObservableService,
-    private router: Router,
     private createAccSrv: CreateAccountService,
     private navCtrl: NavController,
     private authSrv: AuthService,
-    @Inject("nacl.sign") private sign: any
+    private setupPinSrv: SetupPinService
   ) {}
 
   ngOnInit() {
-    //this.checkSetupPin();
     this.generatePassphrase();
-  }
 
-  setupPin(event: any) {
-    this.tempPin = event.pin;
-    this.pagePosition++;
-    this.pageStep++;
-  }
-
-  async confirmPin(event: any) {
-    const pin = event.pin;
-    if (this.tempPin === pin) {
-      this.createAccSrv.setPassphrase(this.passphrase);
-      this.createAccSrv.setPin(pin);
-      await this.createAccSrv.createAccount();
-      const loginStatus = await this.authSrv.login(pin);
-      if (loginStatus) {
-        this.navCtrl.navigateForward("/");
+    this.setupPinSrv.setupPinSubject.subscribe(data => {
+      const { status, pin } = data;
+      if (status == "success") {
+        this.setPin(pin);
       }
-    } else {
-      this.presentToast("Your Pin in not same");
+    });
+  }
+
+  async setPin(pin: string) {
+    this.createAccSrv.setPin(pin);
+    await this.createAccSrv.createAccount();
+    const loginStatus = await this.authSrv.login(pin);
+    if (loginStatus) {
+      this.navCtrl.navigateRoot("main/dashboard");
     }
   }
 
-  setPagePosition(value) {
-    this.pagePosition = value;
+  setupPin() {
+    this.createAccSrv.setPassphrase(this.passphrase);
+    this.navCtrl.navigateForward("setup-pin");
   }
-
-  /*
-  async checkSetupPin() {
-    const pin = await this.storage.get("pin");
-    if (pin) {
-      this.isPinSetup = true;
-    } else {
-      this.isPinSetup = false;
-    }
-  }*/
-
-  /*
-  async goToAccount() {
-    // const pin = await this.storage.get('pin')
-    const savedKey = await this.storage.get("accounts");
-    // const privKeyUint8 = await this.cryptoService.rootKeyFromSeed(this.converterService.hexToArrayByte(this.passphrase))
-    // const pinUint8 = this.converterService.stringToArrayByte(pin)
-
-    // const keyAlgo = {
-    //   name: 'AES-GCM',
-    //   length: 256,
-    // }
-
-    // const opts = {
-    //   format: 'jwk',
-    //   keyAlgo,
-    //   wrapAlgo: {
-    //     name: 'AES-GCM',
-    //     iv: this.cryptoService.genInitVector(),
-    //   },
-    //   deriveAlgo: this.cryptoService.genDeriveAlgo(),
-    // }
-
-    // const wrappedKey = await this.cryptoService.wrapKeyWithPin(privKeyUint8, pinUint8, opts)
-
-    this.storage.set("accounts", [...savedKey, this.account]);
-    this.router.navigate(["/"]);
-  }
-
-  async goToSetupPin() {
-    this.router.navigate(["/setup-pin"]);
-  }*/
 
   async generatePassphrase() {
     const passphrase = this.keyringService.generateRandomPhrase().phrase;
@@ -150,8 +81,6 @@ export class GeneratePassphrasePage implements OnInit {
   }
 
   ionViewDidLeave(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
     this.writtenDown = false;
     this.terms = false;
   }
