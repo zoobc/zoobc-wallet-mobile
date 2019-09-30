b9204aed9708b9bbf98f9a59a3b53c20c646fab6
jhonkus <putukn@gmail.com>
2019-10-01 06:13:48 +0800

    add translation on sidemenu

--- src/app/Components/sidemenu/sidemenu.component.ts
+++ src/app/Components/sidemenu/sidemenu.component.ts
@@ -1,31 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import { MenuController, NavController } from "@ionic/angular";
-import { Router } from "@angular/router";
-import { Storage } from "@ionic/storage";
-import { ObservableService } from "src/app/services/observable.service";
+import { Component, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { ObservableService } from 'src/app/services/observable.service';
 import {
   ACTIVE_ACCOUNT,
   LANGUAGES,
   SELECTED_LANGUAGE,
   CURRENCIES
-} from "src/environments/variable.const";
-import { AccountService } from "src/app/services/account.service";
-import { LanguageService } from "src/app/services/language.service";
-import { CurrencyService } from "src/app/services/currency.service";
-import { ActiveAccountService } from "src/app/services/active-account.service";
+} from 'src/environments/variable.const';
+import { AccountService } from 'src/app/services/account.service';
+import { LanguageService } from 'src/app/services/language.service';
+import { CurrencyService } from 'src/app/services/currency.service';
+import { ActiveAccountService } from 'src/app/services/active-account.service';
 
 @Component({
-  selector: "app-sidemenu",
-  templateUrl: "./sidemenu.component.html",
-  styleUrls: ["./sidemenu.component.scss"]
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.scss']
 })
 export class SidemenuComponent implements OnInit {
   accounts = [];
   languages = [];
-  activeLanguage = "en";
-  activeAccount = "";
+  activeLanguage = 'en';
+  activeAccount = '';
 
-  activeCurrency = "USD";
+  activeCurrency = 'USD';
   currencies = [];
 
   constructor(
@@ -52,49 +52,49 @@ export class SidemenuComponent implements OnInit {
     this.activeLanguage = await this.storage.get(SELECTED_LANGUAGE);
     this.currencies = CURRENCIES;
 
-    const account = await this.storage.get("active_account");
+    const account = await this.storage.get('active_account');
 
     this.activeAccount = account.accountName;
   }
 
   openAboutView() {
-    this.navCtrl.navigateForward("about");
+    this.navCtrl.navigateForward('about');
   }
 
   openListAccount() {
-    this.navCtrl.navigateForward("list-account");
+    this.navCtrl.navigateForward('list-account');
   }
 
   openAddresBook() {
-    this.navCtrl.navigateForward("address-book");
+    this.navCtrl.navigateForward('address-book');
   }
 
   openSendFeedbak() {
-    this.navCtrl.navigateForward("feedback");
+    this.navCtrl.navigateForward('feedback');
   }
 
   openHelpSupport() {
-    this.navCtrl.navigateForward("help");
+    this.navCtrl.navigateForward('help');
   }
 
   openNodeAdmin() {
-    this.navCtrl.navigateForward("node-admin");
+    this.navCtrl.navigateForward('node-admin');
   }
 
   openNotifications() {
-    this.navCtrl.navigateForward("notifications");
+    this.navCtrl.navigateForward('notifications');
   }
 
   openMenu() {
-    this.menuController.open("first");
+    this.menuController.open('first');
   }
 
   goToGenerate() {
-    this.router.navigate(["/create-account"]);
+    this.router.navigate(['/create-account']);
   }
 
   logout() {
-    this.router.navigate(["/login"]);
+    this.router.navigate(['/login']);
   }
 
   selectActiveCurrency() {
@@ -104,13 +104,13 @@ export class SidemenuComponent implements OnInit {
   }
 
   selectActiveLanguage() {
-    console.log("this.activeLanguage", this.activeLanguage);
+    console.log('this.activeLanguage', this.activeLanguage);
     this.languageService.setLanguage(this.activeLanguage);
   }
 
   async getActiveAccount() {
-    const accounts = await this.storage.get("accounts");
-    const account = await this.storage.get("active_account");
+    const accounts = await this.storage.get('accounts');
+    const account = await this.storage.get('active_account');
     accounts.forEach((acc, index) => {
       console.log(
         acc.accountProps.derivationPath === account.accountProps.derivationPath,
