e730d0b219559157ef35f41228a0d0c49d37ef91
jhonkus <putukn@gmail.com>
2019-10-03 14:09:17 +0800

    Add translation in address book

--- src/app/Components/address-book/address-book.module.ts
+++ src/app/Components/address-book/address-book.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { IonicModule } from "@ionic/angular";
-
-import { AddressBookListComponent } from "./address-book-list/address-book-list.component";
-import { AddressBookFormComponent } from "./address-book-form/address-book-form.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { AddressBookListComponent } from './address-book-list/address-book-list.component';
+import { AddressBookFormComponent } from './address-book-form/address-book-form.component';
+import { from } from 'rxjs';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, FormsModule],
+  imports: [CommonModule, IonicModule, FormsModule, TranslateModule],
   declarations: [AddressBookListComponent, AddressBookFormComponent],
   exports: [AddressBookListComponent],
   entryComponents: [AddressBookFormComponent]
 })
-export class AddressBookComponentModule {}
+export class AddressBookComponentModule { }
