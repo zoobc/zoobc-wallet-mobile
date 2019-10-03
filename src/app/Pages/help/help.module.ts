5329ac0aea8210f911f517d68e451c53e4228d4c
jhonkus <putukn@gmail.com>
2019-10-03 14:10:45 +0800

    Add translation for help screen

--- src/app/Pages/help/help.module.ts
+++ src/app/Pages/help/help.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
 
 import { HelpPage } from './help.page';
@@ -17,6 +17,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    TranslateModule,
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
