e82050663f2c614967a4347dddc4386c42ec1e31
jhonkus <putukn@gmail.com>
2019-10-03 14:10:27 +0800

    Add translation for title address book

--- src/app/Pages/address-book/address-book.module.ts
+++ src/app/Pages/address-book/address-book.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { Routes, RouterModule } from "@angular/router";
 
 import { IonicModule } from "@ionic/angular";
-
+import { TranslateModule } from '@ngx-translate/core';
 import { AddressBookPage } from "./address-book.page";
 import { AddressBookComponentModule } from "src/app/Components/address-book/address-book.module";
 
@@ -20,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    TranslateModule,
     RouterModule.forChild(routes),
     AddressBookComponentModule
   ],
