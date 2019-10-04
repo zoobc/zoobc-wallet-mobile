bf96f53d2a617748a5f7072ae6ec9fb98b880a46
jhonkus <putukn@gmail.com>
2019-10-04 20:50:43 +0800

    modify login page to give better error msg

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthService } from "src/app/services/auth-service";
-import { Router } from "@angular/router";
-import { ToastController } from "@ionic/angular";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth-service';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.page.html",
-  styleUrls: ["./login.page.scss"]
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-  private pin = "";
+  private pin = '';
+  public loginFail = false;
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -19,32 +20,31 @@ export class LoginPage implements OnInit {
   ngOnInit() {}
 
   async login(e: any) {
-    const { observer, pin } = e;
+    const { observer, pin, first } = e;
 
+    // set loginFail false && clear error message
+    if (first === true) {
+       this.loginFail = false;
+       return;
+    }
+
+    this.loginFail = false;
     const isUserLoggedIn = await this.authService.login(pin);
     if (isUserLoggedIn) {
-      this.router.navigate(["tabs"]);
-
+      this.router.navigate(['tabs']);
       setTimeout(() => {
         observer.next(true);
       }, 500);
     } else {
       setTimeout(() => {
         observer.next(true);
-        this.failedToast();
-      }, 500);
+        this.loginFail = true;
+       }, 500);
     }
   }
 
   createAccount() {
-    this.router.navigate(["initial"]);
+    this.router.navigate(['initial']);
   }
 
-  async failedToast() {
-    const toast = await this.toastController.create({
-      message: "Unlock Failed",
-      duration: 2000
-    });
-    toast.present();
-  }
 }
