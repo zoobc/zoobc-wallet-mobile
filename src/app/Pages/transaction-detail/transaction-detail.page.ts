73c03d4283a2b0f6470632a7815fdcc4178934dc
jhonkus <putukn@gmail.com>
2019-10-07 04:56:31 +0800

    Acc copy address in transaction detail

--- src/app/Pages/transaction-detail/transaction-detail.page.ts
+++ src/app/Pages/transaction-detail/transaction-detail.page.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { TransactionService } from 'src/app/services/transaction.service';
 import { ActiveAccountService } from 'src/app/services/active-account.service';
 import { AccountService } from 'src/app/services/account.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-transaction-detail',
@@ -28,6 +29,7 @@ export class TransactionDetailPage implements OnInit {
     private activatedRoute: ActivatedRoute,
     private transactionSrv: TransactionService,
     private activeAccountSrv: ActiveAccountService,
+    private toastController: ToastController,
     private accountService: AccountService
   ) {}
 
@@ -60,5 +62,44 @@ export class TransactionDetailPage implements OnInit {
       total: (transactionObj.amount + transactionObj.fee),
       transactionDate: new Date()
     };
+
+    // For testing copy address
+    // this.transaction = {
+    //   id: 'transactionObj.id',
+    //   type: 'transactionObj.type',
+    //   sender: 'transactionObj.sender',
+    //   recipient: 'transactionObj.recipient',
+    //   amount: 10,
+    //   fee: 3,
+    //   total: 13,
+    //   transactionDate: new Date()
+    // };
+    // console.log('====== TRansaction:', this.transaction);
+
   }
+
+  async copyAddress(address:string) {
+
+    console.log('Copy address: ', address);
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = address;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+
+    const toast = await this.toastController.create({
+      message: 'Copied to clipboard.',
+      duration: 2000
+    });
+
+    toast.present();
+}
+
+
 }
