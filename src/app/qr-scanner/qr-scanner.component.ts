007a644583350c595f386a16224c8901b0829941
jhonkus <putukn@gmail.com>
2019-10-11 15:50:23 +0800

    add error handle when scanner not scan barcode but canceled

--- src/app/qr-scanner/qr-scanner.component.ts
+++ src/app/qr-scanner/qr-scanner.component.ts
@@ -12,11 +12,13 @@ import { ActivatedRoute, NavigationExtras } from '@angular/router';
 export class QrScannerComponent implements OnInit {
 
   public from = '';
-
+  isScanned: boolean;
   txtAddress = '';
 
   constructor(private qrScanner: QRScanner, private navCtrl: NavController,
-              private qrScannerSrv: QrScannerService, private activeRoute: ActivatedRoute, private toastController: ToastController) { }
+              private qrScannerSrv: QrScannerService, private activeRoute: ActivatedRoute, private toastController: ToastController){
+
+              }
 
   ngOnInit() {
     this.activeRoute.queryParams.subscribe(params => {
@@ -33,7 +35,7 @@ export class QrScannerComponent implements OnInit {
     } catch (e) {
     }
 
-    if (this.from && this.from === 'tabscan') {
+    if (this.isScanned && this.from && this.from === 'tabscan') {
       // if scanner trigered from tabscan, after scan redirect to scan page.
       const navigationExtras: NavigationExtras = {
         queryParams: {
@@ -43,9 +45,17 @@ export class QrScannerComponent implements OnInit {
       this.navCtrl.navigateForward(['/sendcoin'], navigationExtras);
     }
 
+    this.from = '';
+    this.isScanned = false;
+    this.txtAddress = '';
+
   }
 
   async ionViewDidEnter() {
+
+    this.from = '';
+    this.isScanned = false;
+    this.txtAddress = '';
 
     this.activeRoute.queryParams.subscribe(params => {
       this.from = JSON.parse(params.from);
@@ -59,6 +69,7 @@ export class QrScannerComponent implements OnInit {
           const scanSub = this.qrScanner.scan().subscribe(async (text: string) => {
             this.txtAddress =  text;
             scanSub.unsubscribe();
+            this.isScanned = true;
             this.navCtrl.pop();
           });
           this.qrScanner.show();
@@ -68,11 +79,13 @@ export class QrScannerComponent implements OnInit {
           // then they can grant the permission from there
           this.qrScanner.openSettings();
         } else {
+          this.isScanned = false;
           // permission was denied, but not permanently. You can ask for permission again at a later time.
         }
       })
       .catch((e: any) => {
         alert(e);
+        this.isScanned = false;
         console.log('Error is', e);
       });
 
