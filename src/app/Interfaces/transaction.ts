b840c53adc83cf77a3545ce4de0b1846427497ad
jhonkus <putukn@gmail.com>
2019-10-15 03:52:26 +0800

    change transaction interface add address and timestamp

--- src/app/Interfaces/transaction.ts
+++ src/app/Interfaces/transaction.ts
@@ -1,10 +1,11 @@
 export interface Transaction {
   id: string;
   type: string;
+  address: string;
   sender: string;
   recipient: string;
   amount: number;
   fee: number;
   total: number;
-  transactionDate: Date;
+  timestamp: Date;
 }
