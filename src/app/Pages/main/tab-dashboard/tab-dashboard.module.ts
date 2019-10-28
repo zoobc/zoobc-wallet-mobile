93f416fdf5245552b17dcbcc9e8ef73657ae580c
Ary Atmajaya <aryatmajaya@gmail.com>
2019-10-28 11:30:35 +0800

    update dashboard account name

--- src/app/Pages/main/tab-dashboard/tab-dashboard.module.ts
+++ src/app/Pages/main/tab-dashboard/tab-dashboard.module.ts
@@ -9,6 +9,7 @@ import { Network } from "@ionic-native/network/ngx";
 import { SidemenuModule } from "src/app/Shared/sidemenu/sidemenu.module";
 import { CalendarDateModule } from "src/app/Shared/calendar-date/calendar-date.module";
 import { CurrencySelectionModule } from "src/app/Shared/currency-selection/currency-selection.module";
+import { AddressElipsisModule } from "src/app/Shared/address-elipsis/address-elipsis.module";
 
 @NgModule({
   imports: [
@@ -19,7 +20,8 @@ import { CurrencySelectionModule } from "src/app/Shared/currency-selection/curre
     RouterModule.forChild([{ path: "", component: TabDashboardPage }]),
     SidemenuModule,
     CalendarDateModule,
-    CurrencySelectionModule
+    CurrencySelectionModule,
+    AddressElipsisModule
   ],
   declarations: [TabDashboardPage],
   entryComponents: [],
