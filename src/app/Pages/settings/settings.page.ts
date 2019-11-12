c0c39fb6c0b3ec92f8d032e30909d5cdbff96451
jhonkus <putukn@gmail.com>
2019-11-12 10:03:44 +0800

    cleanup feedback

--- src/app/Pages/settings/settings.page.ts
+++ src/app/Pages/settings/settings.page.ts
@@ -12,4 +12,20 @@ export class SettingsPage implements OnInit {
   ngOnInit() {
   }
 
+  async getCurrencyRates() {
+
+    await this.storage.get(OPENEXCHANGE_CURR_LIST_STORAGE).then((strg) => {
+      const rates = Object.keys(strg).map(currencyName => {
+        const rate = {
+          name: currencyName,
+          value: strg[currencyName],
+        };
+        return rate;
+      });
+      console.log('== Rates on settings: ', rates);
+      this.currencyRates = rates;
+
+    });
+  }
+
 }
