a754ec400b6f58e268e5ea34cdac45d88c2e0a8d
jhonkus <putu.kusuma@blockchainzoo.com>
2019-12-23 13:45:19 +0800

    modify feedback page

--- src/app/Pages/feedback/feedback.module.ts
+++ src/app/Pages/feedback/feedback.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
 
 import { FeedbackPage } from './feedback.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 const routes: Routes = [
   {
@@ -17,10 +18,12 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    TranslateModule,
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [FeedbackPage]
+  declarations: [FeedbackPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FeedbackPageModule {}
