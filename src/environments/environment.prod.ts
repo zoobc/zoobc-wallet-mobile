b056bbe16162dddd54f5f70b75de69f3773974df
jhonkus <putu.kusuma@blockchainzoo.com>
2019-12-23 13:40:36 +0800

    modify variable and environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,14 @@
 export const environment = {
   production: true,
+  defaultCurrency: 'USD',
   currencyRateUrl: 'https://api.exchangeratesapi.io/latest?base=USD',
-  grpcUrl: 'http://18.139.3.139:5001',
+  openExchangeUrl: 'https://openexchangerates.org/api',
+  feedbackUrl: 'https://doorbell.io/api',
+  grpcUrl: '//172.104.47.168:8001',
   mnemonicLanguage: 'english',
   mnemonicNumWords: 24,
   signalID: '790fe50a-589d-40c7-92af-683582d40acf',
   appID: '482874041634',
   zbcPriceInUSD: 10,
-  salt: 'iwillgotomoon'
+  saltForAccount: 'p4ssphr4se',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,18 @@
 
 export const environment = {
   production: false,
-  grpcUrl: 'http://18.139.3.139:5001',
+  defaultCurrency: 'USD',
+  // grpcUrl: 'http://18.139.3.139:5001',
+  grpcUrl: '//172.104.47.168:8001',
+  feedbackUrl: 'https://doorbell.io/api',
   currencyRateUrl: 'https://api.exchangeratesapi.io/latest?base=USD',
+  openExchangeUrl: 'https://openexchangerates.org/api',
   mnemonicLanguage: 'english',
   mnemonicNumWords: 24,
   signalID: '790fe50a-589d-40c7-92af-683582d40acf',
   appID: '482874041634',
   zbcPriceInUSD: 10,
-  salt: 'iwillgotomoon'
+  saltForAccount: 'p4ssphr4se'
+
 };
 
