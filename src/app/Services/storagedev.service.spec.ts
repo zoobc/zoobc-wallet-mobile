db58100d1298dabac925c50a231dd6236f5a0103
jhonkus <putukn@gmail.com>
2020-02-24 15:13:18 +0800

    Add storage service

---
+++ src/app/Services/storagedev.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StoragedevService } from './storagedev.service';
+
+describe('StoragedevService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: StoragedevService = TestBed.get(StoragedevService);
+    expect(service).toBeTruthy();
+  });
+});
