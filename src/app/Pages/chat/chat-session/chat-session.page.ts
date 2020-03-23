289eb6f17b6cdb903475c840d37619edd74861df
jhonkus <putukn@gmail.com>
2020-03-23 08:05:41 +0800

    add function for  get account detail in session page

--- src/app/Pages/chat/chat-session/chat-session.page.ts
+++ src/app/Pages/chat/chat-session/chat-session.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NavParams, IonContent, Events } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chat-session',
@@ -10,8 +11,8 @@ export class ChatSessionPage implements OnInit {
 
   @ViewChild('content') content: IonContent;
   @ViewChild('chat_input') messageInput: ElementRef;
-  User: string = "Me";
-  toUser: string = "driver";
+  User = 'Me';
+  toUser = 'driver';
   inp_text: any;
   editorMsg = '';
   showEmojiPicker = false;
@@ -22,149 +23,165 @@ export class ChatSessionPage implements OnInit {
     time: any,
     message: any,
     upertext: any;
-  }>
+  }>;
+
+  index: number;
+  address = '';
+  name = '';
+  sender = '';
+
   public count = 0;
   public arr = [
     {
-      "messageId": "1",
-      "userId": "140000198202211138",
-      "userName": "Luff",
-      "userImgUrl": "./assets/user.jpg",
-      "toUserId": "210000198410281948",
-      "toUserName": "Hancock",
-      "userAvatar": "./assets/to-user.jpg",
-      "time": 1488349800000,
-      "message": "Hey, that\'s an awesome chat UI",
-      "status": "success"
+      messageId: '1',
+      userId: '140000198202211138',
+      userName: 'Luff',
+      userImgUrl: './assets/user.jpg',
+      toUserId: '210000198410281948',
+      toUserName: 'Hancock',
+      userAvatar: './assets/to-user.jpg',
+      time: 1488349800000,
+      message: 'Hey, thats an awesome chat UI',
+      status: 'success'
 
     },
     {
-      "messageId": "2",
-      "userId": "210000198410281948",
-      "userName": "Hancock",
-      "userImgUrl": "./assets/to-user.jpg",
-      "toUserId": "140000198202211138",
-      "toUserName": "Luff",
-      "userAvatar": "./assets/user.jpg",
-      "time": 1491034800000,
-      "message": "Right, it totally blew my mind. They have other great apps and designs too !",
-      "status": "success"
+      messageId: '2',
+      userId: '210000198410281948',
+      userName: 'Hancock',
+      userImgUrl: './assets/to-user.jpg',
+      toUserId: '140000198202211138',
+      toUserName: 'Luff',
+      userAvatar: './assets/user.jpg',
+      time: 1491034800000,
+      message: 'Right, it totally blew my mind. They have other great apps and designs too !',
+      status: 'success'
     },
     {
-      "messageId": "3",
-      "userId": "140000198202211138",
-      "userName": "Luff",
-      "userImgUrl": "./assets/user.jpg",
-      "toUserId": "210000198410281948",
-      "toUserName": "Hancock",
-      "userAvatar": "./assets/to-user.jpg",
-      "time": 1491034920000,
-      "message": "And it is free ?",
-      "status": "success"
+      messageId: '3',
+      userId: '140000198202211138',
+      userName: 'Luff',
+      userImgUrl: './assets/user.jpg',
+      toUserId: '210000198410281948',
+      toUserName: 'Hancock',
+      userAvatar: './assets/to-user.jpg',
+      time: 1491034920000,
+      message: 'And it is free ?',
+      status: 'success'
     },
     {
-      "messageId": "4",
-      "userId": "210000198410281948",
-      "userName": "Hancock",
-      "userImgUrl": "./assets/to-user.jpg",
-      "toUserId": "140000198202211138",
-      "toUserName": "Luff",
-      "userAvatar": "./assets/user.jpg",
-      "time": 1491036720000,
-      "message": "Yes, totally free. Beat that ! ",
-      "status": "success"
+      messageId: '4',
+      userId: '210000198410281948',
+      userName: 'Hancock',
+      userImgUrl: './assets/to-user.jpg',
+      toUserId: '140000198202211138',
+      toUserName: 'Luff',
+      userAvatar: './assets/user.jpg',
+      time: 1491036720000,
+      message: 'Yes, totally free. Beat that ! ',
+      status: 'success'
     },
     {
-      "messageId": "5",
-      "userId": "210000198410281948",
-      "userName": "Hancock",
-      "userImgUrl": "./assets/to-user.jpg",
-      "toUserId": "140000198202211138",
-      "toUserName": "Luff",
-      "userAvatar": "./assets/user.jpg",
-      "time": 1491108720000,
-      "message": "Wow, that\'s so cool. Hats off to the developers. This is gooood stuff",
-      "status": "success"
+      messageId: '5',
+      userId: '210000198410281948',
+      userName: 'Hancock',
+      userImgUrl: './assets/to-user.jpg',
+      toUserId: '140000198202211138',
+      toUserName: 'Luff',
+      userAvatar: './assets/user.jpg',
+      time: 1491108720000,
+      message: 'Wow, thats so cool. Hats off to the developers. This is gooood stuff',
+      status: 'success'
     },
     {
-      "messageId": "6",
-      "userId": "140000198202211138",
-      "userName": "Luff",
-      "userImgUrl": "./assets/user.jpg",
-      "toUserId": "210000198410281948",
-      "toUserName": "Hancock",
-      "userAvatar": "./assets/to-user.jpg",
-      "time": 1491231120000,
-      "message": "Check out their other designs.",
-      "status": "success"
+      messageId: '6',
+      userId: '140000198202211138',
+      userName: 'Luff',
+      userImgUrl: './assets/user.jpg',
+      toUserId: '210000198410281948',
+      toUserName: 'Hancock',
+      userAvatar: './assets/to-user.jpg',
+      time: 1491231120000,
+      message: 'Check out their other designs.',
+      status: 'success'
     }
-  ]
+  ];
 
 
 
-  constructor(private events: Events, ) {
+  constructor(private events: Events,
+              private activeRoute: ActivatedRoute) {
+
+    this.activeRoute.queryParams.subscribe(params => {
+      console.log('=== Params: ', params);
+      this.index = params.idx;
+      this.sender = params.sender;
+      this.name = params.name;
+      this.address = params.address;
+    });
+
     this.msgList = [
       {
         userId: this.User,
         userName: this.User,
-        userAvatar: "assets/driver.jpeg",
-        time: "12:01 pm",
+        userAvatar: 'assets/driver.jpeg',
+        time: '12:01 pm',
         message: 'Hey, that\'s an awesome chat UI',
         upertext: 'Hello'
       },
       {
         userId: this.toUser,
         userName: this.toUser,
-        userAvatar: "assets/user.jpeg",
-        time: "12:01 pm",
-        message: "Right, it totally blew my mind. They have other great apps and designs too!",
-        upertext: "Hii"
+        userAvatar: 'assets/user.jpeg',
+        time: '12:01 pm',
+        message: 'Right, it totally blew my mind. They have other great apps and designs too!',
+        upertext: 'Hii'
       },
       {
         userId: this.User,
         userName: this.User,
-        userAvatar: "assets/driver.jpeg",
-        time: "12:01 pm",
+        userAvatar: 'assets/driver.jpeg',
+        time: '12:01 pm',
         message: 'And it is free ?',
         upertext: 'How r u '
       },
       {
         userId: this.toUser,
         userName: this.toUser,
-        userAvatar: "assets/user.jpeg",
-        time: "12:01 pm",
+        userAvatar: 'assets/user.jpeg',
+        time: '12:01 pm',
         message: 'Yes, totally free. Beat that !',
         upertext: 'good'
       },
       {
         userId: this.User,
         userName: this.User,
-        userAvatar: "assets/driver.jpeg",
-        time: "12:01 pm",
+        userAvatar: 'assets/driver.jpeg',
+        time: '12:01 pm',
         message: 'Wow, that\'s so cool. Hats off to the developers. This is gooood stuff',
         upertext: 'How r u '
       },
       {
         userId: this.toUser,
         userName: this.toUser,
-        userAvatar: "assets/user.jpeg",
-        time: "12:01 pm",
+        userAvatar: 'assets/user.jpeg',
+        time: '12:01 pm',
         message: 'Check out their other designs.',
         upertext: 'good'
       },
       {
         userId: this.User,
         userName: this.User,
-        userAvatar: "assets/driver.jpeg",
-        time: "12:01 pm",
+        userAvatar: 'assets/driver.jpeg',
+        time: '12:01 pm',
         message: 'Have you seen their other apps ? They have a collection of ready-made apps for developers. This makes my life so easy. I love it! ',
         upertext: 'How r u '
       },
       {
         userId: this.toUser,
         userName: this.toUser,
-        userAvatar: "assets/user.jpeg",
-        time: "12:01 pm",
+        userAvatar: 'assets/user.jpeg',
+        time: '12:01 pm',
         message: 'Well, good things come in small package after all',
         upertext: 'good'
       },
@@ -221,20 +238,20 @@ export class ChatSessionPage implements OnInit {
     this.msgList.push({
       userId: this.User,
       userName: this.User,
-      userAvatar: "assets/user.jpeg",
-      time: "12:01 pm",
+      userAvatar: 'assets/user.jpeg',
+      time: '12:01 pm',
       message: this.inp_text,
       upertext: this.inp_text
     })
     this.msgList.push({
       userId: this.toUser,
       userName: this.toUser,
-      userAvatar: "assets/user.jpeg",
-      time: "12:01 pm",
+      userAvatar: 'assets/user.jpeg',
+      time: '12:01 pm',
       message: otherUser,
       upertext: otherUser
     });
-    this.inp_text = "";
+    this.inp_text = '';
     console.log('scrollBottom');
     setTimeout(() => {
       this.scrollToBottom()
