177a89e3624ccca00c32b2f55c856c4bec926e58
jhonkus <putukn@gmail.com>
2020-04-07 11:40:04 +0800

    Add chat models

---
+++ src/app/Models/chatmodels.ts
@@ -0,0 +1,13 @@
+export interface User {
+    email: string;
+    address: string;
+    name: string;
+    time: string;
+}
+
+export interface Chat {
+    message: string;
+    pair: string;
+    sender: string;
+    time: number;
+}
