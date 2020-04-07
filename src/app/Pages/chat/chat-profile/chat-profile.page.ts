6233d6ab06a9c949e8f5a6b094dbba43e124b863
jhonkus <putukn@gmail.com>
2020-04-07 11:38:30 +0800

    Add chat profile page

---
+++ src/app/Pages/chat/chat-profile/chat-profile.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-profile',
+  templateUrl: './chat-profile.page.html',
+  styleUrls: ['./chat-profile.page.scss'],
+})
+export class ChatProfilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
