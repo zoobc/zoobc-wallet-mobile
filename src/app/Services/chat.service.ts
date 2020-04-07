26cba9424ba90a27e1ff7573193191b3b9344970
jhonkus <putukn@gmail.com>
2020-04-07 11:39:15 +0800

    modify chat service using new function

--- src/app/Services/chat.service.ts
+++ src/app/Services/chat.service.ts
@@ -1,109 +1,112 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-import { FIREBASE_CHAT } from 'src/environments/variable.const';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { FIREBASE_CHAT, FIREBASE_CHAT_USER } from 'src/environments/variable.const';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { firestore } from 'firebase/app';
 import { map, switchMap } from 'rxjs/operators';
 import { Observable, combineLatest, of } from 'rxjs';
+import { User, Chat } from '../Models/chatmodels';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor(
-    private afAuth: AngularFireAuth,
-    private afs: AngularFirestore,
-    private router: Router
-  ) { }
+  // constructor(
+  //   private afAuth: AngularFireAuth,
+  //   private afs: AngularFirestore,
+  //   private router: Router
+  // ) { }
 
 
-  async create() {
-    const { uid } = await firebase.auth().currentUser;
-    const data = {
-      uid,
-      createdAt: Date.now(),
-      count: 0,
-      messages: []
-    };
+  // async create() {
+  //   const { uid } = await firebase.auth().currentUser;
+  //   const data = {
+  //     uid,
+  //     createdAt: Date.now(),
+  //     count: 0,
+  //     messages: []
+  //   };
 
-    const docRef = await this.afs.collection(FIREBASE_CHAT).add(data);
+  //   const docRef = await this.afs.collection(FIREBASE_CHAT).add(data);
 
-    return docRef;
+  //   return docRef;
+  // }
+
+  // async sendMessage(chatId, content) {
+  //   const { uid } = await firebase.auth().currentUser;
+
+  //   const data = {
+  //     uid,
+  //     content,
+  //     createdAt: Date.now()
+  //   };
+
+  //   if (uid) {
+  //     const ref = this.afs.collection(FIREBASE_CHAT).doc(chatId);
+  //     return ref.update({
+  //       messages: firestore.FieldValue.arrayUnion(data)
+  //     });
+  //   }
+  // }
+
+
+
+
+
+
+  // private updateUserData({ uid, email, displayName, photoURL }) {
+  //   const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${uid}`);
+
+  //   const data = {
+  //     uid,
+  //     email,
+  //     displayName,
+  //     photoURL
+  //   };
+
+  //   return userRef.set(data, { merge: true });
+  // }
+
+
+
+  // ====
+
+  users: AngularFirestoreCollection<User>;
+  private userDoc: AngularFirestoreDocument<User>;
+
+  chats: AngularFirestoreCollection<Chat>;
+  private chatDoc: AngularFirestoreDocument<Chat>;
+
+  //The pair string for the two users currently chatting
+  currentChatPairId;
+  currentChatPairId2;
+  currentChatPartner;
+
+  constructor(private db: AngularFirestore) {
+    //Get the tasks collecction
+    this.users = db.collection<User>(FIREBASE_CHAT_USER);
+    this.chats = db.collection<Chat>(FIREBASE_CHAT);
   }
 
-  async sendMessage(chatId, content) {
-    const { uid } = await firebase.auth().currentUser;
-
-    const data = {
-      uid,
-      content,
-      createdAt: Date.now()
-    };
-
-    if (uid) {
-      const ref = this.afs.collection(FIREBASE_CHAT).doc(chatId);
-      return ref.update({
-        messages: firestore.FieldValue.arrayUnion(data)
-      });
-    }
+  addUser(payload) {
+    return this.users.add(payload);
   }
 
-  get(chatId) {
-    return this.afs
-      .collection<any>(FIREBASE_CHAT)
-      .doc(chatId)
-      .snapshotChanges()
-      .pipe(
-        map(doc => {
-          return { id: doc.payload.id, data: doc.payload.data };
-        })
-      );
+  addChat(chat: Chat) {
+    return this.chats.add(chat);
   }
 
-  joinUsers(chat$: Observable<any>) {
-    let chat;
-    const joinKeys = {};
-
-    return chat$.pipe(
-      switchMap(c => {
-        // Unique User IDs
-        chat = c;
-
-        console.log('==== Chat: ', chat);
-        
-        const uids = Array.from(new Set(c.messages.map(v => v.uid)));
-
-        // Firestore User Doc Reads
-        const userDocs = uids.map(u =>
-          this.afs.doc(`users/${u}`).valueChanges()
-        );
-
-        return userDocs.length ? combineLatest(userDocs) : of([]);
-      }),
-      map(arr => {
-        arr.forEach(v => (joinKeys[(<any>v).uid] = v));
-        chat.messages = chat.messages.map(v => {
-          return { ...v, user: joinKeys[v.uid] };
-        });
-
-        return chat;
-      })
-    );
+  createPairId(user1, user2) {
+    const pairId = `${user1.address}|${user2.address}`;
+    return pairId;
   }
 
-  private updateUserData({ uid, email, displayName, photoURL }) {
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${uid}`);
-
-    const data = {
-      uid,
-      email,
-      displayName,
-      photoURL
-    };
-
-    return userRef.set(data, { merge: true });
+  createPairId2(user1, user2) {
+    const pairId2 = `${user2.address}|${user1.address}`;
+    return pairId2;
   }
+
 }
