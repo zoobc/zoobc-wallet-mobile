ce552aa4021bb1e52987b2c7572b3ca7cd483cc1
jhonkus <putukn@gmail.com>
2020-05-18 12:11:13 +0800

    rearange location of address book module

--- src/app/Components/address-book/address-book-list/address-book.module.ts
+++ src/app/Components/address-book/address-book-list/address-book.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { AddressBookListComponent } from './address-book-list/address-book-list.component';
-import { from } from 'rxjs';
+import { AddressBookListComponent } from './address-book-list.component';
 
 @NgModule({
   imports: [CommonModule, IonicModule, FormsModule, TranslateModule],
