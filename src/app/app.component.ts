253856320b2e9de9723402ac89ad8c2821154848
jhonkus <putu.kusuma@blockchainzoo.com>
2020-05-23 13:48:43 +0800

    Add dark mode function in app compoent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,7 +50,21 @@ export class AppComponent implements OnInit {
     private currencyService: CurrencyService,
     private theme: ThemeService  ) {
     this.initializeApp();
+    // this.darkMode();
+  }
 
+  darkMode() {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    toggleDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+
+    // Add or remove the "dark" class based on if the media query matches
+    function toggleDarkTheme(shouldAdd: boolean) {
+      document.body.classList.toggle('dark', shouldAdd);
+    }
   }
 
   initializeApp() {
