51fd38eae41794abecb9feb989fd645f2c3a361a
jhonkus <putukn@gmail.com>
2020-09-18 17:55:29 +0800

    change key of list node keys

--- src/environments/variable.const.ts
+++ src/environments/variable.const.ts
@@ -2,7 +2,7 @@ import { Currency } from 'src/app/Interfaces/currency';
 export const COIN_CODE = 'ZBC';
 export const SALT_PASSPHRASE = '';
 export const STORAGE_ACTIVE_CURRENCY = 'active_currency';
-export const STORAGE_ACTIVE_NETWORK_IDX = 'current_node_selected1';
+export const STORAGE_ACTIVE_NETWORK_IDX = 'current_node_selected2';
 export const STORAGE_CURRENCY_RATES = 'currency_rates';
 export const STORAGE_CURRENCY_RATE = 'rate';
 export const STORAGE_ADDRESS_BOOK = 'address_book';
@@ -71,160 +71,160 @@ export const LANGUAGES = [
     img: 'assets/images/lang/ar.png'
   }
   ,
-  {
-    country: 'ελληνικά',
-    code: 'el',
-    img: 'assets/images/lang/el.png'
-  },
-  {
-    country: 'Dansk',
-    code: 'da',
-    img: 'assets/images/lang/da.png'
-  },
-  {
-    country: 'Deutsche',
-    code: 'de',
-    img: 'assets/images/lang/de.png'
-  },
+  // {
+  //   country: 'ελληνικά',
+  //   code: 'el',
+  //   img: 'assets/images/lang/el.png'
+  // },
+  // {
+  //   country: 'Dansk',
+  //   code: 'da',
+  //   img: 'assets/images/lang/da.png'
+  // },
+  // {
+  //   country: 'Deutsche',
+  //   code: 'de',
+  //   img: 'assets/images/lang/de.png'
+  // },
   {
     country: 'English',
     code: 'en',
     img: 'assets/images/lang/en.png'
   },
-  {
-    country: 'Español',
-    code: 'es',
-    img: 'assets/images/lang/es.png'
-  },
-  {
-    country: 'Suomi',
-    code: 'fi',
-    img: 'assets/images/lang/fi.png'
-  },
-  {
-    country: 'Français',
-    code: 'fr',
-    img: 'assets/images/lang/fr.png'
-  },
-  {
-    country: 'हिन्दी',
-    code: 'hi',
-    img: 'assets/images/lang/hi.png'
-  },
-  {
-    country: 'Hebrew',
-    code: 'he',
-    img: 'assets/images/lang/he.png'
-  },
-  {
-    country: 'Hrvatski',
-    code: 'hr',
-    img: 'assets/images/lang/hr.png'
-  },
-  {
-    country: 'Hungarian',
-    code: 'hu',
-    img: 'assets/images/lang/hu.png'
-  },
+  // {
+  //   country: 'Español',
+  //   code: 'es',
+  //   img: 'assets/images/lang/es.png'
+  // },
+  // {
+  //   country: 'Suomi',
+  //   code: 'fi',
+  //   img: 'assets/images/lang/fi.png'
+  // },
+  // {
+  //   country: 'Français',
+  //   code: 'fr',
+  //   img: 'assets/images/lang/fr.png'
+  // },
+  // {
+  //   country: 'हिन्दी',
+  //   code: 'hi',
+  //   img: 'assets/images/lang/hi.png'
+  // },
+  // {
+  //   country: 'Hebrew',
+  //   code: 'he',
+  //   img: 'assets/images/lang/he.png'
+  // },
+  // {
+  //   country: 'Hrvatski',
+  //   code: 'hr',
+  //   img: 'assets/images/lang/hr.png'
+  // },
+  // {
+  //   country: 'Hungarian',
+  //   code: 'hu',
+  //   img: 'assets/images/lang/hu.png'
+  // },
   {
     country: 'Indonesia',
     code: 'id',
     img: 'assets/images/lang/id.png'
   },
-  {
-    country: 'Italiano',
-    code: 'it',
-    img: 'assets/images/lang/it.png'
-  },
-  {
-    country: '한국어',
-    code: 'ko',
-    img: 'assets/images/lang/ko.png'
-  },
-  {
-    country: 'Malay',
-    code: 'ms',
-    img: 'assets/images/lang/ms.png'
-  },
-  {
-    country: 'Nederlands',
-    code: 'nl',
-    img: 'assets/images/lang/nl.png'
-  },
-  {
-    country: 'Norwegian',
-    code: 'no',
-    img: 'assets/images/lang/no.png'
-  },
-  {
-    country: 'Português',
-    code: 'pt',
-    img: 'assets/images/lang/pt.png'
-  },
-  {
-    country: '日本語',
-    code: 'ja',
-    img: 'assets/images/lang/ja.png'
-  },
-  {
-    country: 'Русский',
-    code: 'ru',
-    img: 'assets/images/lang/ru.png'
-  },
-  {
-    country: 'Slovenčina',
-    code: 'sl',
-    img: 'assets/images/lang/sl.png'
-  },
-  {
-    country: 'Swedish',
-    code: 'sv',
-    img: 'assets/images/lang/sv.png'
-  },
-  {
-    country: 'Polskie',
-    code: 'pl',
-    img: 'assets/images/lang/pl.png'
-  },
-  {
-    country: 'Limba română',
-    code: 'ro',
-    img: 'assets/images/lang/ro.png'
-  },
-  {
-    country: '中文(简体)',
-    code: 'zh',
-    img: 'assets/images/lang/zh.png'
-  },
-  {
-    country: 'ไทย',
-    code: 'th',
-    img: 'assets/images/lang/th.png'
-  },
-  {
-    country: 'Tiếng Việt',
-    code: 'vi',
-    img: 'assets/images/lang/vi.png'
-  },
-  {
-    country: 'Türkçe',
-    code: 'tr',
-    img: 'assets/images/lang/tr.png'
-  }
+  // {
+  //   country: 'Italiano',
+  //   code: 'it',
+  //   img: 'assets/images/lang/it.png'
+  // },
+  // {
+  //   country: '한국어',
+  //   code: 'ko',
+  //   img: 'assets/images/lang/ko.png'
+  // },
+  // {
+  //   country: 'Malay',
+  //   code: 'ms',
+  //   img: 'assets/images/lang/ms.png'
+  // },
+  // {
+  //   country: 'Nederlands',
+  //   code: 'nl',
+  //   img: 'assets/images/lang/nl.png'
+  // },
+  // {
+  //   country: 'Norwegian',
+  //   code: 'no',
+  //   img: 'assets/images/lang/no.png'
+  // },
+  // {
+  //   country: 'Português',
+  //   code: 'pt',
+  //   img: 'assets/images/lang/pt.png'
+  // },
+  // {
+  //   country: '日本語',
+  //   code: 'ja',
+  //   img: 'assets/images/lang/ja.png'
+  // },
+  // {
+  //   country: 'Русский',
+  //   code: 'ru',
+  //   img: 'assets/images/lang/ru.png'
+  // },
+  // {
+  //   country: 'Slovenčina',
+  //   code: 'sl',
+  //   img: 'assets/images/lang/sl.png'
+  // },
+  // {
+  //   country: 'Swedish',
+  //   code: 'sv',
+  //   img: 'assets/images/lang/sv.png'
+  // },
+  // {
+  //   country: 'Polskie',
+  //   code: 'pl',
+  //   img: 'assets/images/lang/pl.png'
+  // },
+  // {
+  //   country: 'Limba română',
+  //   code: 'ro',
+  //   img: 'assets/images/lang/ro.png'
+  // },
+  // {
+  //   country: '中文(简体)',
+  //   code: 'zh',
+  //   img: 'assets/images/lang/zh.png'
+  // },
+  // {
+  //   country: 'ไทย',
+  //   code: 'th',
+  //   img: 'assets/images/lang/th.png'
+  // },
+  // {
+  //   country: 'Tiếng Việt',
+  //   code: 'vi',
+  //   img: 'assets/images/lang/vi.png'
+  // },
+  // {
+  //   country: 'Türkçe',
+  //   code: 'tr',
+  //   img: 'assets/images/lang/tr.png'
+  // }
 ];
 
 export const NETWORK_LIST = [
   {
     name: 'Beta 1',
-    host: 'http://n1.beta.proofofparticipation.network:7001/'
+    host: 'http://n1.beta.proofofparticipation.network:7001'
   },
   {
     name: 'Beta 2',
-    host: 'http://n2.beta.proofofparticipation.network:7001/'
+    host: 'http://n2.beta.proofofparticipation.network:7001'
   },
   {
     name: 'Beta 3',
-    host: 'http://n3.beta.proofofparticipation.network:7001/'
+    host: 'http://n3.beta.proofofparticipation.network:7001'
   },
   {
     name: 'Alpha',
