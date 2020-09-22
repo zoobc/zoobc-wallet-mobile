e8e90b6ca46173d493ce6cd3661a0e9e761825ec
jhonkus <putukn@gmail.com>
2020-09-22 16:28:42 +0800

    update account dataset

--- src/app/Pages/account/dataset-account/dataset-account.page.ts
+++ src/app/Pages/account/dataset-account/dataset-account.page.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { Account } from 'src/app/Interfaces/account';
+import { Currency } from 'src/app/Interfaces/currency';
+import { AccountService } from 'src/app/Services/account.service';
+import { CurrencyService } from 'src/app/Services/currency.service';
+import { TRANSACTION_MINIMUM_FEE } from 'src/environments/variable.const';
+import { truncate } from 'src/Helpers/utils';
+import zoobc, { AccountDatasetListParams, AccountDatasetsResponse, BIP32Interface, RemoveDatasetInterface } from 'zoobc-sdk';
 
 @Component({
   selector: 'app-dataset-account',
@@ -6,10 +17,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dataset-account.page.scss'],
 })
 export class DatasetAccountPage implements OnInit {
+  account: Account;
 
-  constructor() { }
+  subscription: Subscription = new Subscription();
+  dataSetList: any[];
+  dataSet: any;
+  isLoading: boolean;
+  isError: boolean;
+  isLoadingDelete: boolean;
+  isErrorDelete: boolean;
+  minFee = TRANSACTION_MINIMUM_FEE;
+  currencyRate: Currency;
+  formDataset: FormGroup;
+  feeForm = new FormControl(this.minFee, [Validators.required, Validators.min(this.minFee)]);
+  feeFormCurr = new FormControl('', Validators.required);
+  typeFeeField = new FormControl('ZBC');
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private currencyServ: CurrencyService,
+    private authServ: AccountService,
+    private translate: TranslateService
+  ) {
+    this.formDataset = new FormGroup({
+      fee: this.feeForm,
+      feeCurr: this.feeFormCurr,
+      typeFee: this.typeFeeField,
+    });
+  }
 
   ngOnInit() {
+    console.log('== account: ', this.account);
+    // if currency changed
+    const subsRate = this.currencyServ.currencySubject.subscribe((rate: Currency) => {
+      this.currencyRate = rate;
+      const minCurrency = truncate(this.minFee * rate.value, 8);
+      this.feeFormCurr.patchValue(minCurrency);
+      this.feeFormCurr.setValidators([Validators.required, Validators.min(minCurrency)]);
+    });
+    this.subscription.add(subsRate);
+
+    this.activeRoute.queryParams.subscribe(params => {
+      this.account = JSON.parse(params.account);
+      this.getDataSetList();
+    });
+
+  }
+
+  getDataSetList() {
+    this.isError = false;
+    this.isLoading = true;
+    const listParam: AccountDatasetListParams = {
+      recipientAccountAddress: this.account.address,
+    };
+    zoobc.AccountDataset.getList(listParam)
+      .then((res: AccountDatasetsResponse) => {
+        this.dataSetList = res.accountdatasetsList;
+        console.log('== this dataset: ', this.dataSetList);
+      })
+      .catch(err => {
+        this.isError = true;
+        console.log(err);
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
+
+
+  onRefresh() {
+    this.getDataSetList();
   }
 
 }
