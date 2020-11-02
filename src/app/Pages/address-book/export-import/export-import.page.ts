d26a8684a9027e142ee72983b7e140441a821b8c
jhonkus <putukn@gmail.com>
2020-11-02 16:04:28 +0800

    Modify export import contacts

--- src/app/Pages/address-book/export-import/export-import.page.ts
+++ src/app/Pages/address-book/export-import/export-import.page.ts
@@ -11,6 +11,9 @@ import { AlertController, NavController } from '@ionic/angular';
   styleUrls: ['./export-import.page.scss']
 })
 export class ExportImportPage implements OnInit {
+
+  addresses = '';
+
   constructor(
     private addressBookSrv: AddressBookService,
     private utilService: UtilService,
@@ -18,24 +21,57 @@ export class ExportImportPage implements OnInit {
     private navCtrl: NavController
   ) {}
 
-  addresses = '';
   async ngOnInit() {
-    const alladdress = await this.addressBookSrv.getAll();
-    if (alladdress) {
-      this.addresses = JSON.stringify(
-        alladdress.map((addrs: any) => {
-          return {
-            name: addrs.name,
-            address: addrs.address
-          };
-        })
-      );
-    }
+    // const alladdress = await this.addressBookSrv.getAll();
+    // if (alladdress) {
+    //   this.addresses = JSON.stringify(
+    //     alladdress.map((addrs: any) => {
+    //       return {
+    //         name: addrs.name,
+    //         address: addrs.address
+    //       };
+    //     })
+    //   );
+    // }
   }
 
   copy() {
     this.utilService.copyToClipboard(this.addresses);
   }
+
+  public uploadFile(files: FileList) {
+    if (files && files.length > 0) {
+      const file = files.item(0);
+      const fileReader: FileReader = new FileReader();
+      fileReader.readAsText(file, 'JSON');
+      fileReader.onload = async () => {
+        try {
+          const arrAddresses: any[] = JSON.parse(fileReader.result.toString());
+          const addresses: Contact[] = [];
+          for (let i = 0; i < arrAddresses.length; i++) {
+            const { name, address } = arrAddresses[i];
+            if (name && address) {
+              const contact: Contact = {
+                name,
+                address: sanitizeString(address)
+              };
+              addresses.push(contact);
+            }
+          }
+          await this.addressBookSrv.update(addresses);
+
+        } catch (error) {
+          const alert = await this.alertCtrl.create({
+            header: 'Error',
+            message: 'You have entered wrong json format',
+            buttons: ['OK']
+          });
+          await alert.present();
+        }
+      };
+    }
+  }
+
 
   async save() {
     try {
