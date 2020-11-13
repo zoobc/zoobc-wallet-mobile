6442ab3b68ce70f17b81d0df7edd3a31fe9a415e
komodo2662 <thomas.prabowo@blockchainzoo.com>
2020-11-13 10:43:25 +0800

    notification, qr-scanner, tabs, theme, transaction

--- src/app/Pages/tabs/tabs.page.spec.ts
+++ src/app/Pages/tabs/tabs.page.spec.ts
@@ -1,26 +1,33 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TabsPage } from './tabs.page';
+import {TranslateModule } from '@ngx-translate/core';
 
-// describe('TabsPage', () => {
-//   let component: TabsPage;
-//   let fixture: ComponentFixture<TabsPage>;
+describe('TabsPage', () => {
+  let component: TabsPage;
+  let fixture: ComponentFixture<TabsPage>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [TabsPage],
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA]
-//     }).compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TabsPage],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports:[
+          TranslateModule.forRoot()
+      ]
+    }).compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(TabsPage);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TabsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+//   it("tabs change worked", () => {
+//     let result = component.tabChanged();
+//     expect(Array.isArray(result)).toBeTruthy;
 //   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+});
--- src/app/Pages/theme/theme.page.spec.ts
+++ src/app/Pages/theme/theme.page.spec.ts
@@ -1,27 +1,42 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ThemePage } from './theme.page';
+import {TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IonicStorageModule } from '@ionic/storage';
 
-// describe('ThemePage', () => {
-//   let component: ThemePage;
-//   let fixture: ComponentFixture<ThemePage>;
+describe('ThemePage', () => {
+  let component: ThemePage;
+  let fixture: ComponentFixture<ThemePage>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ThemePage ],
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ThemePage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports:[
+          TranslateModule.forRoot(),
+          RouterTestingModule,
+          IonicStorageModule.forRoot()
+      ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ThemePage);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ThemePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+    it("init worked", () => {
+    let result = component.ngOnInit();
+    expect(Array.isArray(result)).toBeTruthy;
+  });
+    it("select theme worked", () => {
+    let result = component.selectTheme("1");
+    expect(Array.isArray(result)).toBeTruthy;
+  });
+});
