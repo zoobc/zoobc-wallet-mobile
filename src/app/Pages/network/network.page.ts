53346fd60b9e1b20feec595b9fe49a301860b160
jhonkus <putukn@gmail.com>
2021-01-03 19:46:26 +0800

    add network form to add new network

--- src/app/Pages/network/network.page.ts
+++ src/app/Pages/network/network.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NETWORK_LIST } from 'src/environments/variable.const';
-import { NavController } from '@ionic/angular';
+import { AlertController, NavController, PopoverController } from '@ionic/angular';
 import { NetworkService } from 'src/app/Services/network.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ZbcNetwork } from 'src/app/Interfaces/zbc-network';
+import { PopoverOptionComponent } from 'src/app/Components/popover-option/popover-option.component';
 
 @Component({
   selector: 'app-network',
@@ -9,20 +11,163 @@ import { NetworkService } from 'src/app/Services/network.service';
   styleUrls: ['./network.page.scss'],
 })
 export class NetworkPage implements OnInit {
-
-  constructor(private navCtrl: NavController, private networkSrv: NetworkService) { }
-
-  networks = NETWORK_LIST;
+  form: FormGroup;
+  networks = [];
   activeNetwork = null;
+  fName = new FormControl('', Validators.required);
+  fHost = new FormControl('', [Validators.required, Validators.pattern('^https?://+[\\w.-]+:\\d+$')]);
+  actionMode = 'add';
+  idx = 0;
+  isShowForm = false;
+  textEdit = 'edit';
+  textDelete = 'delete';
+
+  constructor(
+    private navCtrl: NavController,
+    private popoverCtrl: PopoverController,
+    private alertController: AlertController,
+    private networkSrv: NetworkService
+  ) { }
+
+
 
   async ngOnInit() {
+    this.networks = this.networkSrv.getall();
+    this.form = new FormGroup({
+      name: this.fName,
+      host: this.fHost
+    });
     this.activeNetwork = await this.networkSrv.getNetwork();
   }
 
-  selectNetwork(index: any){
-    this.networkSrv.setNetwork(index)
+  selectNetwork(index: any) {
+    this.networkSrv.setNetwork(index);
     this.networkSrv.broadcastSelectNetwork(this.networks[index]);
     this.navCtrl.pop();
+  }
+
+  get errorControl() {
+    return this.form.controls;
+  }
+
+  addHost() {
+    this.actionMode = 'add';
+    this.showForm();
+  }
+
+  showForm() {
+    this.isShowForm = true;
+  }
+
+  edit(idx: number) {
+    this.actionMode = 'edit';
+    this.idx = idx;
+    this.isShowForm = true;
+    const network =   this.networks[this.idx];
+    this.fName.setValue(network.name);
+    this.fHost.setValue(network.host);
+  }
+
+  async delete(idx: number) {
+    this.idx = idx;
+    this.networks.splice(this.idx, 1);
+    console.log('== this.networks after delte:', this.networks);
+    await this.networkSrv.saveAll(this.networks);
+  }
+
+  close() {
+    this.isShowForm = false;
+  }
+
+  async deleteConfirmation() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure <strong>to delete</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.delete(this.idx);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async save(): Promise<void> {
+
+    this.fHost.markAsTouched();
+    this.fName.markAsTouched();
+
+    if (!this.form.valid) {
+      console.log('Please provide all the required values!');
+      return;
+    }
+
+    const newHost: ZbcNetwork = {
+      name: this.fName.value,
+      host: this.fHost.value,
+      default: false,
+    };
+
+    console.log('=== newHost: ', newHost);
+    if (this.actionMode === 'add') {
+      console.log(' == will add', newHost);
+      await this.networkSrv.add(newHost);
+    } else if (this.actionMode === 'edit') {
+      console.log(' == will edit', newHost);
+      this.networks[this.idx] = newHost;
+      await this.networkSrv.saveAll(this.networks);
+    }
+    this.networks = this.networkSrv.getall();
+    console.log('== Sekarang: ', this.form.value);
+    this.isShowForm = false;
+  }
+
+  async showOption(ev: any, index: number) {
+
+    this.idx = index;
+    const popoverOptions = [
+      {
+        key: 'edit',
+        label: this.textEdit
+      },
+      {
+        key: 'delete',
+        label: this.textDelete
+      }
+    ];
+
+    const popover = await this.popoverCtrl.create({
+      component: PopoverOptionComponent,
+      componentProps: {
+        options: popoverOptions
+      },
+      event: ev,
+      translucent: true
+    });
+
+    popover.onWillDismiss().then(({ data: action }) => {
+      switch (action) {
+        case 'edit':
+          this.edit(index);
+          break;
+        case 'delete':
+          this.deleteConfirmation();
+          break;
+      }
+    });
+
+    return popover.present();
   }
 
 }
--- src/app/Services/network.service.ts
+++ src/app/Services/network.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from './storage.service';
 import zoobc from 'zbc-sdk';
-import { STORAGE_ACTIVE_NETWORK_IDX, NETWORK_LIST } from 'src/environments/variable.const';
+import { STORAGE_ACTIVE_NETWORK_IDX, NETWORK_LIST, STORAGE_ALL_NETWORKS } from 'src/environments/variable.const';
 import { Subject } from 'rxjs';
+import { ZbcNetwork } from '../Interfaces/zbc-network';
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
 
   public nodeIndex = 0;
+  public allNetorks: ZbcNetwork[];
   constructor(
     private strgSrv: StorageService,
   ) { }
   public changeNodeSubject: Subject<any> = new Subject<any>();
 
   setInitialNetwork() {
-    zoobc.Network.list(NETWORK_LIST);
+
+    this.strgSrv.getObject(STORAGE_ALL_NETWORKS).then((val: ZbcNetwork[]) => {
+      if (!val || val === null) {
+        console.log('val1: ', val);
+        this.allNetorks = NETWORK_LIST;
+        this.saveAll(this.allNetorks);
+        zoobc.Network.list(this.allNetorks);
+      } else {
+        console.log('val2: ', val);
+        this.allNetorks = val;
+        zoobc.Network.list(this.allNetorks);
+      }
+    });
+
     this.strgSrv.get(STORAGE_ACTIVE_NETWORK_IDX).then((val: any) => {
       if (!val || val === null) {
         this.setNetwork(0);
@@ -23,8 +38,22 @@ export class NetworkService {
         this.setNetwork(val);
       }
     });
-
   }
+
+  async add(network: ZbcNetwork) {
+    this.allNetorks.push(network);
+    zoobc.Network.list(this.allNetorks);
+    await this.saveAll(this.allNetorks);
+  }
+
+  getall() {
+    return this.allNetorks;
+  }
+
+  async saveAll(all: ZbcNetwork[]) {
+    await this.strgSrv.setObject(STORAGE_ALL_NETWORKS, all);
+  }
+
 
   async setNetwork(idx: number) {
     this.nodeIndex = idx;
