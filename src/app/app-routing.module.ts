06eee1d64da73c27bd7d3422c55e5ff849c941fe
jhonkus <putukn@gmail.com>
2021-03-15 16:19:11 +0800

    update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -255,7 +255,10 @@ const routes: Routes = [
     canActivate: [AuthService]
   },
   { path: 'msig-detail', loadChildren: './Pages/multisig/msig-detail/msig-detail.module#MsigDetailPageModule',
-    canActivate: [AuthService] },  { path: 'restore-acc', loadChildren: './Pages/wallet/restore-acc/restore-acc.module#RestoreAccPageModule' }
+    canActivate: [AuthService] },
+  { path: 'restore-acc', loadChildren: './Pages/wallet/restore-acc/restore-acc.module#RestoreAccPageModule' },
+  { path: 'escrow-history', loadChildren: './Pages/my-tasks/escrow-history/escrow-history.module#EscrowHistoryPageModule' }
+
 
 
 
